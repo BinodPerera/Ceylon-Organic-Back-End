a5efc937071b11fce3842a82a6f0e68a0305459c
Binod-Perera <108719707+BinodPerera@users.noreply.github.com>
2025-03-27 20:27:49 +0530

    port num

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ app.use("/api/category", categoryRoutes);
 const productRoutes = require("./routes/productRoutes");
 app.use("/api/product", productRoutes);
 
+app.use("/api/serverstatus", async ( req, res ) => {
+  res.status(200).json({ message: "Server is running" });
+})
+
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
