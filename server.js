49d540998bde14cf6bd9c09a29ca0e1271ba175c
Binod-Perera <108719707+BinodPerera@users.noreply.github.com>
2025-03-29 21:29:36 +0530

    envirement variables set

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cookieParser()); // Reason: To store JWT in cookies
 // Middleware - Enable CORS to allow cookies
 app.use(
   cors({
-    origin: "https://ceylon-organic-front-end.vercel.app",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
